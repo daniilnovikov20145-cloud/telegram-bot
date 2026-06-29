710b955726a205543da2bd7c9ece9b968ae6a601
daniilnovikov20145-cloud <daniil.novikov.20145@gmail.com>
2026-06-29 15:27:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 
 TOKEN = "8212324861:AAHGxT5eIKx1q6TRghBpSxe4TPX5PTxAF40"
 
-GROUP_LINK = "https://t.me/+2795LTeslD9iOWFi"
+GROUP_LINK = "https://t.me/+z3VEXtWslTVlYmEy"
 WEBSITE_LINK = "https://insightorba.com/"
 PERSONAL_LINK = "https://t.me/vekluch"
 
@@ -33,7 +33,7 @@ VACANCY_TEXT = """
 Під час конференції ми детально розповімо про компанію та відповімо на всі питання 🙋
 
 Ось посилання на групу, де є вся інформація та буде доступ до зустрічі 👇
-https://t.me/+2795LTeslD9iOWFi
+https://t.me/+z3VEXtWslTVlYmEy
 """
 
 bot = Bot(token=TOKEN)
